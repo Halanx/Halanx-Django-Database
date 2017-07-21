9c1b88638860566c37e5ae5f1aa8849059c8bc97
Ayush Gupta <gupta.ayush11786@gmail.com>
2017-07-21 20:20:20 +0530

    New requirements1.txt

--- Products/serializers.py
+++ Products/serializers.py
@@ -5,7 +5,14 @@ from StoreBase.serializers import StoreSerializer, StoreSerializer1
 
 class ProductSerializer(serializers.ModelSerializer):
 
-    RelatedStore = StoreSerializer1()
+    RelatedStore = StoreSerializer1(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductPostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
@@ -13,9 +20,17 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ProductPhotoSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = ProductPhoto
         fields = '__all__'
+
+
+
+
+
+
+
 
 
 
